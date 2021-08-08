b4c837f14b74f4c0568f9ae20725005cc116d5f8
Darren Nickerson <darrennickerson30@gmail.com>
2021-08-08 20:00:03 -0300

    basic functionality of the app

--- js/script.js
+++ js/script.js
@@ -11,57 +11,46 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-const ul = document.querySelector('.student-list');
 
-
-
-const createLi = () => {
-   for(let i = 0; i < data.length; i++) {
-      student = data[i];
-      const li = document.createElement('li');
-      li.className = 'student-item cf';
-      const div = document.createElement('div');
-      div.className = 'student-details';
-      
-      const headingThree = document.createElement('h3');
-      headingThree.textContent = `${student.name.title}  ${student.name.first} ${student.name.last}`;
-      const span = document.createElement('span');
-      span.className = 'email';
-      span.innerHTML = student.email;
-      const divJoined = document.createElement('div');
-      divJoined.className = 'joined-details';
-      divJoined.innerHTML = `<span class="date">${student.registered.date}</span>`;
-      let img = document.createElement('img');
-      const createImg = (src, alt, class_name) => {
-            img.setAttribute('src', src);
-            img.setAttribute('alt', alt);
-            img.className = class_name; 
-      };
-
-      createImg(student.picture.medium, 'Profile Picture', 'avatar');
-      
-      
-      ul.appendChild(li);
-      li.appendChild(div);
-      div.appendChild(img);
-      
-      img.parentNode.insertBefore(headingThree, img.nextSibling);
-      headingThree.parentNode.insertBefore(span, headingThree.nextSibling);
-      span.parentNode.insertBefore(divJoined, span.nextSibling);
-      
-
-   }
-};
-createLi();
-
+// createLi();
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
-const showPage = () => {
 
-}
+
+const showPage = (pageNumber, studentList) => {
+pageNumber = parseInt(pageNumber);
+   startIndex = (pageNumber * 9) - 9;
+   endIndex = pageNumber * 9
+
+   const ul = document.querySelector('.student-list');
+   ul.innerHTML = ''; 
+
+   for (let i = 0; i < studentList.length; i++) {
+      if(i >= startIndex && i < endIndex){
+         student = studentList[i];
+            //Card Goes in here (Template Literal)
+            let html = `  <li class="student-item cf">
+            <div class="student-details">
+              <img class="avatar" src="${student.picture.large}" alt="Profile Picture">
+              <h3>${student.name.first} ${student.name.last}</h3>
+              <span class="email">${student.email}</span>
+            </div>
+            <div class="joined-details">
+              <span class="date">Joined ${student.registered.date}</span>
+            </div>
+          </li>
+`        
+
+            ul.insertAdjacentHTML('beforeend', html)
+      };
+   
+   };
+
+};
+
 
 
 /*
@@ -69,6 +58,27 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+const addPagination = () => {
 
+   const pagination = document.querySelector('.link-list');
+   pageNumbers = Math.ceil(data.length / 9);
+
+   for (let i = 1; i < pageNumbers + 1; i++ )  {
+   const li = document.createElement('li');
+   pagination.appendChild(li);
+   li.innerHTML =  `<button type="button">${i}</button> ` //add active class 
+     
+   }
+   const li = document.querySelector('.link-list');
+   li.addEventListener('click', (e)  => {
+      
+      let number = e.target.textContent;
+      showPage(number, data);
+      window.scrollTo(0,0);
+  });
+
+}
 
 // Call functions
+showPage(1, data);
+addPagination();
