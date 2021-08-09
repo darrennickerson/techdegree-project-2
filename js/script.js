02173f05b8ac17b5a1e86c5b1f1130e1e4ba1f8e
Darren Nickerson <darrennickerson30@gmail.com>
2021-08-09 16:22:25 -0300

    Started the exceeds requirements

--- js/script.js
+++ js/script.js
@@ -3,14 +3,11 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-
 /*
 For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-
 
 // createLi();
 /*
@@ -18,67 +15,108 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+const numberPerpage = 9;
 
+const showPage = (studentList, pageNumber) => {
+  const ul = document.querySelector(".student-list");
+  pageNumber = parseInt(pageNumber);
+  startIndex = pageNumber * numberPerpage - numberPerpage;
+  endIndex = pageNumber * numberPerpage;
+  ul.innerHTML = "";
 
-const showPage = (pageNumber, studentList) => {
-pageNumber = parseInt(pageNumber);
-   startIndex = (pageNumber * 9) - 9;
-   endIndex = pageNumber * 9
+  for (let i = 0; i < studentList.length; i++) {
+    if (i >= startIndex && i < endIndex) {
+      student = studentList[i];
+      //Card Goes in here (Template Literal)
+      let html = `<li class="student-item cf">
+                        <div class="student-details">
+                           <img class="avatar" src="${student.picture.large}" alt="Profile Picture">
+                           <h3>${student.name.first} ${student.name.last}</h3>
+                           <span class="email">${student.email}</span>
+                        </div>
+                           <div class="joined-details">
+                           <span class="date">Joined ${student.registered.date}</span>
+                        </div>
+                     </li> `;
 
-   const ul = document.querySelector('.student-list');
-   ul.innerHTML = ''; 
-
-   for (let i = 0; i < studentList.length; i++) {
-      if(i >= startIndex && i < endIndex){
-         student = studentList[i];
-            //Card Goes in here (Template Literal)
-            let html = `  <li class="student-item cf">
-            <div class="student-details">
-              <img class="avatar" src="${student.picture.large}" alt="Profile Picture">
-              <h3>${student.name.first} ${student.name.last}</h3>
-              <span class="email">${student.email}</span>
-            </div>
-            <div class="joined-details">
-              <span class="date">Joined ${student.registered.date}</span>
-            </div>
-          </li>
-`        
-
-            ul.insertAdjacentHTML('beforeend', html)
-      };
-   
-   };
-
+      ul.insertAdjacentHTML("beforeend", html);
+    }
+  }
 };
-
-
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
-const addPagination = () => {
+const ul = document.querySelector(".link-list");
+const addPagination = (data) => {
+  pageNumbers = Math.ceil(data.length / numberPerpage);
 
-   const pagination = document.querySelector('.link-list');
-   pageNumbers = Math.ceil(data.length / 9);
+  for (let i = 1; i < pageNumbers + 1; i++) {
+    const li = document.createElement("li");
+    ul.appendChild(li);
+    li.innerHTML = `<button type="button">${i}</button> `; //add active class
+  }
 
-   for (let i = 1; i < pageNumbers + 1; i++ )  {
-   const li = document.createElement('li');
-   pagination.appendChild(li);
-   li.innerHTML =  `<button type="button">${i}</button> ` //add active class 
-     
-   }
-   const li = document.querySelector('.link-list');
-   li.addEventListener('click', (e)  => {
-      
-      let number = e.target.textContent;
-      showPage(number, data);
-      window.scrollTo(0,0);
+  ul.addEventListener("click", (e) => {
+    let number = e.target.textContent;
+    showPage(data, number);
+    window.scrollTo(0, 0);
+  });
+};
+
+// create searchBox
+
+const searchBox = (data) => {
+  html = `<label for="search" class="student-search">
+   <span>Search by name</span>
+   <input id="search" placeholder="Search by name...">
+   <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+ </label>`;
+  const header = document.querySelector(".header");
+  header.insertAdjacentHTML("beforeend", html);
+
+  // put all names in an array
+  let studentNames = [];
+  for (let i = 0; i < data.length; i++) {
+    let student = data[i];
+
+    let studentName = student.name.first + " " + student.name.last;
+
+    studentNames.push(studentName.toLowerCase());
+  }
+  // get search input
+
+  let search = document.querySelector("#search");
+  let searchValue = "";
+  search.addEventListener("keyup", (e) => {
+    searchValue = e.target.value;
+
+    console.log(searchValue);
+    let searchResults = [];
+    searchResults = studentNames.filter((x) => x.includes(searchValue));
+    let objects = [];
+    let firstName;
+    let lastName;
+    if (searchResults.length < data.length) {
+      console.log(`This is it ${searchResults.length}`);
+      for (let i = 0; i < searchResults.length; i++) {
+        let split = searchResults[i].split(" ");
+        firstName = split[0];
+        let lastName = split[1];
+      }
+      let object = data.find(
+        (obj) => obj.name.first.toLowerCase() === firstName
+      );
+      console.log(object);
+    }
   });
 
-}
+  console.log(studentNames);
+};
 
 // Call functions
-showPage(1, data);
-addPagination();
+searchBox(data);
+showPage(data, 1);
+addPagination(data);
